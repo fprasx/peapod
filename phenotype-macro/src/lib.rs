59fb32bef7391918ce084d4a09658b0663503f31
fprasx <felix725@gmail.com>
2022-07-16 19:29:55 -0400

    refactored making auxiliary structs

--- phenotype-macro/src/lib.rs
+++ phenotype-macro/src/lib.rs
@@ -1,18 +1,19 @@
 use proc_macro::TokenStream;
 use proc_macro_error::{abort, proc_macro_error};
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput, FieldsNamed, FieldsUnnamed};
+use std::collections::HashMap;
+use syn::{parse_macro_input, DeriveInput, FieldsNamed, FieldsUnnamed, Ident, Variant};
+
+const U8_MAX: usize = u8::MAX as usize;
+const U16_MAX: usize = u16::MAX as usize;
+const U8_MAX_PLUS_1: usize = u8::MAX as usize + 1;
+const U16_MAX_PLUS_1: usize = u16::MAX as usize + 1;
+const U32_MAX: usize = u32::MAX as usize;
+const NOTE: &str = "can only derive phenotype on enums";
 
 #[proc_macro_derive(phenotype)]
 #[proc_macro_error]
 pub fn phenotype(input: TokenStream) -> TokenStream {
-    const U8_MAX: usize = ::std::u8::MAX as usize;
-    const U16_MAX: usize = ::std::u16::MAX as usize;
-    const U8_MAX_PLUS_1: usize = ::std::u8::MAX as usize + 1;
-    const U16_MAX_PLUS_1: usize = ::std::u16::MAX as usize + 1;
-    const U32_MAX: usize = ::std::u32::MAX as usize;
-    const NOTE: &str = "can only derive phenotype on enums";
-
     let ast = parse_macro_input!(input as DeriveInput);
     let ident = ast.ident.clone();
 
@@ -29,7 +30,8 @@ pub fn phenotype(input: TokenStream) -> TokenStream {
 
     let variants = ast.variants;
 
-    // What type should the discriminant be?
+    // Which integer type should the discriminant be?
+    // Select the smallest type that can hold all variants
     let discriminant_ty = match variants.len() {
         0..=U8_MAX => quote! { u8 },
         U8_MAX_PLUS_1..=U16_MAX => quote! { u16 },
@@ -37,68 +39,65 @@ pub fn phenotype(input: TokenStream) -> TokenStream {
         _ => quote! { usize },
     };
 
-    // Match tags to discriminants
-    let discriminants = variants
+    // Store the tags as keys and the variants as values
+    let mut map = HashMap::new();
+    map.reserve(variants.len());
+
+    for (tag, variant) in variants.into_iter().enumerate() {
+        map.insert(tag, variant);
+    }
+
+    // Zip tags together with discriminants
+    // Each quote! looks something like `ident::variant => 4u8,`
+    let as_tags = map
         .iter()
-        .enumerate()
-        // The cast is ok as the match discriminant_ty's init mean sit can hold enough variants
-        .map(|(tag, variant)| quote! { #ident::#variant => #tag as #discriminant_ty,})
-        .collect::<Vec<_>>();
+        // Cast is safe as `discriminant_ty` is init'd to be big enough
+        .map(|(tag, variant)| quote! { #ident::#variant => #tag as #discriminant_ty,});
 
-    // Make the union that holds the data
+    // Define the union that holds the data
     let union_ident = format_ident!("__{}Data", ident);
-    let mut union_fields = ::std::vec::Vec::new();
-    let mut struct_defs = ::std::vec::Vec::new();
-    for var in variants.iter() {
-        let field = &var.ident;
-        let enum_field_data = match var.fields.clone() {
-            // Create a dummy struct that contains the named fields
-            syn::Fields::Named(FieldsNamed { named, .. }) => {
-                let sname = format_ident!("__{}{}Data", ident, field);
-                let idents = named.iter().map(|field| field.ident.as_ref().unwrap());
-                let types = named.iter().map(|field| &field.ty);
-                struct_defs.push(quote! {
-                    struct #sname {
-                        #(#idents: #types),*
-                    }
-                });
-                quote! { ::std::mem::ManuallyDrop<#sname> }
+    let mut union_fields = Vec::new();
+    let mut struct_defs = Vec::new();
+    for key in map.keys() {
+        let variant = map.get(key).unwrap();
+
+        let field = variant.ident.clone();
+
+        match def_auxiliary_struct(variant, &ident) {
+            Some(Auxiliary { ident, tokens }) => {
+                // We're going to need this later
+                struct_defs.push(tokens);
+
+                // Add a struct field to the union
+                union_fields.push(quote! {
+                    #field: ::std::mem::ManuallyDrop<#ident>
+                })
             }
-
-            // Create a dummy tuple struct that contains the fields
-            syn::Fields::Unnamed(FieldsUnnamed { unnamed, .. }) => {
-                let sname = format_ident!("__{}{}Data", ident, field);
-                let types = unnamed.iter().map(|field| &field.ty);
-                struct_defs.push(quote! { struct #sname(#(#types),*); });
-                quote! { ::std::mem::ManuallyDrop<#sname> }
+            None => {
+                // Add a struct field to the union
+                union_fields.push(quote! {
+                    #field: () // TODO: this might not be the right thing to represent "no data"
+                })
             }
-
-            // No fields so we don't need to do anything
-            syn::Fields::Unit => quote! { () }, // TODO: this might not be right
-        };
-
-        // Add a struct field to the union
-        union_fields.push(quote! {
-            #field: #enum_field_data
-        })
+        }
     }
 
     let value = quote! {
-        #(#struct_defs)*
+        #(struct #struct_defs)*
         #[allow(non_snake_case)]
         union #union_ident {
             #(#union_fields),*
         }
     };
 
-    let x = quote! {
+    quote! {
         #value
         impl phenotype_internal::Phenotype for #ident {
             type Value = #union_ident;
             type Discriminant = #discriminant_ty;
             fn discriminant(&self) -> Self::Discriminant {
                 match &self {
-                    #(#discriminants)*
+                    #(#as_tags)*
                 }
             }
 
@@ -110,7 +109,45 @@ pub fn phenotype(input: TokenStream) -> TokenStream {
                 todo!()
             }
         }
-    };
-    println!("{x}");
-    x.into()
+    }.into()
+}
+
+struct Auxiliary {
+    ident: Ident,
+    tokens: proc_macro2::TokenStream,
+}
+
+/// Return an auxilliary struct that can hold the data from an enum variant.
+/// Returns `None` if the variant doesn't contain any data
+fn def_auxiliary_struct(variant: &Variant, enum_name: &Ident) -> Option<Auxiliary> {
+    let field = &variant.ident;
+    match &variant.fields {
+        // Create a dummy struct that contains the named fields
+        syn::Fields::Named(FieldsNamed { named, .. }) => {
+            let struct_name = format_ident!("__{}{}Data", enum_name, field);
+            let idents = named.iter().map(|field| field.ident.as_ref().unwrap());
+            let types = named.iter().map(|field| &field.ty);
+            Some(Auxiliary {
+                ident: struct_name.clone(),
+                tokens: quote! {
+                    #struct_name {
+                        #(#idents: #types),*
+                    }
+                },
+            })
+        }
+
+        // Create a dummy tuple struct that contains the fields
+        syn::Fields::Unnamed(FieldsUnnamed { unnamed, .. }) => {
+            let struct_name = format_ident!("__{}{}Data", enum_name, field);
+            let types = unnamed.iter().map(|field| &field.ty);
+            Some(Auxiliary {
+                ident: struct_name.clone(),
+                tokens: quote! { #struct_name(#(#types),*); },
+            })
+        }
+
+        // No fields so we don't need to do anything
+        syn::Fields::Unit => None, 
+    }
 }
