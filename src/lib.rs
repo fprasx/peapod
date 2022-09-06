f2e4609dfb57c36952b10c06c3b4caf92003f548
Felix Prasanna <fpx@mit.edu>
2022-09-06 00:54:12 -0400

    added pub use for phenotype_* crates

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 #![no_std]
 
 // Exports :)
-extern crate phenotype_internal;
-extern crate phenotype_macro;
 pub use crate::peapod::Peapod;
+pub use phenotype_internal;
+pub use phenotype_macro;
 pub use phenotype_internal::{Phenotype, PhenotypeDebug};
 pub use phenotype_macro::{Phenotype, PhenotypeDebug};
 
